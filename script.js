41d6c3f9563a163547b48cdcd4125a38b067195d
novidandiana <febyanaaaretnoo@gmail.com>
2026-09-03 14:26:16 +0700

    Pratikum XI RPL 1

---
+++ script.js
@@ -0,0 +1,16 @@
+function tampilkanNama(){
+    document.getElementById("namaAnggota").innerHTML=
+    `
+
+    <ol> 
+        <style="list-style-type: decimal;
+        padding-left:5%;">
+        <li>Retno (Feby@gmail.com)</li>
+        <li>Fiya (Fiya@gmail.com)</li>
+    </ol>
+
+        <button onclick="location.reload()">
+            Tutup Kembali
+        </button>
+ `;
+}
